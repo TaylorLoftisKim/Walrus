ac21f027024723e4bad359afe105a55c78b85390
Erica Nafziger and Taylor Loftis-Kim <erica@>
2016-10-18 09:15:28 -0700

    add sea otter clickable features to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function()  {
-  $(".clickable").click(function() {
+  $(".clickable1").click(function() {
     $("#initially-hidden").toggle();
     $("#initially-showing").toggle();
+  });
+
+  $(".clickable2").click(function() {
+    $("#initially-hidden-otter").toggle();
+    $("#initially-showing-otter").toggle();
   });
 });
